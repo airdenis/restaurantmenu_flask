02aaae806e3e812c4a6b24e6ece948d38b98de0f
vagrant <vagrant@vagrant.vm>
2018-10-16 01:14:36 +0000

    feat: add json api aplication

--- project.py
+++ project.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, redirect, request, url_for, flash
+from flask import (
+        Flask, render_template, redirect, request, url_for, flash, jsonify
+    )
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -76,7 +78,20 @@ def deleteMenuItem(restaurant_id, menu_id):
                 item=deleteItem
                 )
 
-    return "page to delete a menu item. Task 3 complete!"
+
+@app.route('/restaurants/<int:restaurant_id>/menu/JSON/')
+def restaurantMenuJSON(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(
+            restaurant_id=restaurant.id
+            ).all()
+    return jsonify(MenuItems=[i.serialize for i in items])
+
+
+@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/JSON/')
+def restaurantMenuItemJSON(restaurant_id, menu_id):
+    item = session.query(MenuItem).filter_by(id=menu_id).one()
+    return jsonify(MenuItem=item.serialize)
 
 
 if __name__ == '__main__':
